9945c7c5ead75a1d00b0dcdfc68d697e1ad089cb
Alexey Kalinin <camradal@yahoo.com>
2013-01-14 19:52:59 -0800

    Initial commit

---
+++ onthisday.js/js/data.js
@@ -0,0 +1,110 @@
+﻿(function () {
+    "use strict";
+
+    var currentDate = new Date();
+    var list = new WinJS.Binding.List();
+    var groupedItems = list.createGrouped(
+        function groupKeySelector(item) { return item.group.key; },
+        function groupDataSelector(item) { return item.group; }
+    );
+
+    loadData();
+
+    WinJS.Namespace.define("Data", {
+        items: groupedItems,
+        groups: groupedItems.groups,
+        getItemReference: getItemReference,
+        getItemsFromGroup: getItemsFromGroup,
+        resolveGroupReference: resolveGroupReference,
+        resolveItemReference: resolveItemReference
+    });
+
+    // Get a reference for an item, using the group key and item title as a
+    // unique reference to the item that can be easily serialized.
+    function getItemReference(item) {
+        return [item.group.key, item.title];
+    }
+
+    // This function returns a WinJS.Binding.List containing only the items
+    // that belong to the provided group.
+    function getItemsFromGroup(group) {
+        return list.createFiltered(function (item) { return item.group.key === group.key; });
+    }
+
+    // Get the unique group corresponding to the provided group key.
+    function resolveGroupReference(key) {
+        for (var i = 0; i < groupedItems.groups.length; i++) {
+            if (groupedItems.groups.getAt(i).key === key) {
+                return groupedItems.groups.getAt(i);
+            }
+        }
+    }
+
+    // Get a unique item from the provided string array, which should contain a
+    // group key and an item title.
+    function resolveItemReference(reference) {
+        for (var i = 0; i < groupedItems.length; i++) {
+            var item = groupedItems.getAt(i);
+            if (item.group.key === reference[0] && item.title === reference[1]) {
+                return item;
+            }
+        }
+    }
+    
+    var parseData = function (data, group) {
+        var text = data.parse.text['*'];
+        var entries = onthisday.extractEntries(text);
+        var items = [];
+        for (var i = 0; i < entries.length; i++) {
+            var entry = entries[i];
+            items.push({
+                group: group,
+                title: entry.year,
+                subtitle: '',
+                description: entry.description,
+                content: '',
+                backgroundImage: null,
+                links: entry.links
+            });
+        }
+        return items;
+    };
+
+    function loadData(date) {
+        if (!date) {
+            date = currentDate;
+        }
+        date = onthisday.formatDate(date);
+        
+        var groups = [
+            { key: "group1", title: "Highlights", backgroundImage: null, description: 'Highlights' },
+            { key: "group2", title: "Events", backgroundImage: null, description: 'Events' },
+            { key: "group3", title: "Births", backgroundImage: null, description: 'Births' },
+            { key: "group4", title: "Deaths", backgroundImage: null, description: 'Deaths' }
+        ];
+
+        var promises = [
+            onthisday.getHighlights(date).then(function (data) {
+                return parseData(data, groups[0]);
+            }),
+            onthisday.getEvents(date).then(function (data) {
+                return parseData(data, groups[1]);
+            }),
+            onthisday.getBirths(date).then(function (data) {
+                return parseData(data, groups[2]);
+            }),
+            onthisday.getDeaths(date).then(function (data) {
+                return parseData(data, groups[3]);
+            })
+        ];
+
+        WinJS.Promise.join(promises).done(function (data) {
+            // now push items into the list
+            for (var i = 0; i < data.length; i++) {
+                for (var j = 0; j < data[i].length; j++) {
+                    list.push(data[i][j]);
+                }
+            }
+        });
+    }
+})();
---
+++ onthisday.js/js/onthisday.js
@@ -0,0 +1,115 @@
+﻿var onthisday = function () {
+    "use strict";
+
+    var highlightsUrl = 'http://en.wikipedia.org/w/api.php?action=parse&prop=text&format=json&page=Wikipedia:Selected_anniversaries/';
+    var eventsUrl = 'http://en.wikipedia.org/w/api.php?action=parse&prop=text&format=json&section=1&page=';
+    var birthsUrl = 'http://en.wikipedia.org/w/api.php?action=parse&prop=text&format=json&section=2&page=';
+    var deathsUrl = 'http://en.wikipedia.org/w/api.php?action=parse&prop=text&format=json&section=3&page=';
+
+    var getHighlights = function (date) { return getEntries(highlightsUrl, date); };
+    var getEvents = function (date) { return getEntries(eventsUrl, date); };
+    var getBirths = function (date) { return getEntries(birthsUrl, date); };
+    var getDeaths = function (date) { return getEntries(deathsUrl, date); };
+
+    var getEntries = function (url, date) {
+        url = url + date;
+        return $.get(url);
+    };
+
+    var getContent = function (title) {
+        return $.ajax({
+            url: 'https://en.wikipedia.org/w/api.php',
+            data: {
+                action: 'mobileview',
+                page: title,
+                sections: 'all',
+                format: 'json'
+            }
+        });
+    };
+
+    var getImage = function (title) {
+        return $.ajax({
+            url: 'https://en.wikipedia.org/w/api.php',
+            data: {
+                action: 'query',
+                titles: title,
+                prop: 'imageinfo',
+                generator: 'images',
+                iiprop: 'url',
+                format: 'json'
+            }
+        });
+    };
+
+    var extractEntries = function (text) {
+        var items = [];
+        var dom = $(text);
+        $('li', dom).each(function (i, el) {
+            var split = el.innerText.split(' – ', 2);
+            if (split.length === 2) {
+                var links = findLinks(el);
+                items.push({
+                    year: split[0],
+                    description: split[1],
+                    links: links
+                });
+            }
+        });
+
+        return items;
+    };
+
+    var findLinks = function (ctx) {
+        var boldLink;
+        var links = [];
+        $('b a', ctx).each(function (i, el) {
+            boldLink = {
+                title: el.title,
+                description: el.innerText
+            };
+            links.push(boldLink);
+        });
+        $('a', ctx).each(function (i, el) {
+            var link = {
+                title: el.title,
+                description: el.innerText
+            };
+            links.push(link);
+        });
+        return links;
+    };
+
+    function formatDate(date) {
+        var wrapper = moment(date);
+        return wrapper.format('MMMM_D');
+    }
+
+    function incrementDate(date) {
+        return moment(date).add('days', 1).toString();
+    }
+
+    function decrementDate(date) {
+        return moment(date).subtract('days', 1).toString();
+    }
+    
+    function format(text) {
+        text = text.replace(/\/\//g, 'http://');
+        text = text.replace(/\/wiki\//g, 'http://en.wikipedia.org/wiki/');
+        return text;
+    }
+
+    return {
+        getHighlights: getHighlights,
+        getEvents: getEvents,
+        getBirths: getBirths,
+        getDeaths: getDeaths,
+        extractEntries: extractEntries,
+        getContent: getContent,
+        formatDate: formatDate,
+        incrementDate: incrementDate,
+        decrementDate: decrementDate,
+        getImage: getImage,
+        format: format
+    };
+}();
